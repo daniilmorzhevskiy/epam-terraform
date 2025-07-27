a0aab6549738dc77e5528accff7193a382715583
danylomorzhevskyi <danylo.morzhevskyi@goreel.tech>
2025-07-27 12:41:47 +0200

    att1 T2

---
+++ ssh.tf
@@ -0,0 +1,9 @@
+resource "aws_key_pair" "cmtr_keypair" {
+  key_name = "cmtr-dmg42ceb-keypair"
+  public_key = var.ssh_key
+
+  tags = {
+    Project = "epam-tf-lab"
+    ID = "cmtr-dmg42ceb"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "ssh_key" {
+  desctiption = "SSH key for accessing the instance"
+  type = string
+}
