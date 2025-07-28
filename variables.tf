6ad3036e79a03d78998d7a2ad590000dd18dbceb
Daniil Morzhevskiy <daniilmorzhevskiy@MacBookAir-2.home>
2025-07-28 17:30:48 +0200

    adjustmetns

--- variables.tf
+++ variables.tf
@@ -7,9 +7,3 @@ variable "aws_secret_key" {
   description = "AWS secret key"
   type        = string
 }
-
-variable "project_tag" {
-  description = "Project tag for all resources"
-  type        = string
-  default     = "cmtr-dmg42ceb"
-}
