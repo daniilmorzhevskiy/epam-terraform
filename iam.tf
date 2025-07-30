4da89f7d11c673babf499a2906d446f5ecc37db9
Daniil Morzhevskiy <daniilmorzhevskiy@MacBookAir-2.home>
2025-07-30 21:22:08 +0200

    new version of tf

--- iam.tf
+++ iam.tf
@@ -1,9 +1,48 @@
+resource "aws_iam_group" "group" {
+  name = "${var.project_tag}-iam-group"
+  tags = {
+    Project = var.project_tag
+  }
+}
+
 resource "aws_iam_policy" "policy" {
   name        = "${var.project_tag}-iam-policy"
   description = "Write-only access to the S3 bucket"
   policy = templatefile("${path.module}/policy.json", {
     bucket_name = var.bucket_name
   })
+  tags = {
+    Project = var.project_tag
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name = "${var.project_tag}-iam-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+  tags = {
+    Project = var.project_tag
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_iam_instance_profile" "profile" {
+  name = "${var.project_tag}-iam-instance-profile"
+  role = aws_iam_role.role.name
   tags = {
     Project = var.project_tag
   }
