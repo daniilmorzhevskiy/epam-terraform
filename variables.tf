6db4a890058f8af3948d5c490e7a40fc8ba36517
Daniil Morzhevskiy <daniilmorzhevskiy@MacBookAir-2.home>
2025-07-30 21:46:36 +0200

    test

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-}
+# variable "aws_access_key" {
+#   description = "AWS access key"
+#   type        = string
+# }
 
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-}
+# variable "aws_secret_key" {
+#   description = "AWS secret key"
+#   type        = string
+# }
 
 variable "project_tag" {
   description = "Project tag for resource naming"
